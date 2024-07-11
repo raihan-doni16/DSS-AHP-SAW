6b9274938b6b9efda3e3ee67a26feb1f58cf4283
afifghz <afifghaniizahran@gmail.com>
2024-07-12 06:04:01 +0700

    edit style 2

--- resources/views/component/sidebar.blade.php
+++ resources/views/component/sidebar.blade.php
@@ -1,16 +1,17 @@
 
-<aside class="relative bg-sidebar h-screen w-64 hidden sm:block shadow-xl">
-    <div class="p-6">
-        <img src="{{asset('img/logo-UT.png')}}" alt="">
+<aside class="relative h-screen w-64 hidden sm:block shadow-xl">
+    <div class="p-6 h-1/6">
+        <img src="{{asset('img/logo/logo-full.svg')}}" alt="">
     </div>
-    <a href="{{ route('home') }}" class="flex items-center text-white py-4 pl-6 {{ request()->routeIs('home') ? 'active-nav-link' : '' }}">
-        <i class="fas fa-tachometer-alt mr-3"></i>
-        Dashboard
-    </a>
+    <div class="bg-green-600 text-white h-5/6 rounded-xl">
+        <a href="{{ route('home') }}" class="flex items-center text-white py-4 pl-6 opacity-75 hover:opacity-100 nav-item {{ request()->routeIs('home') ? 'active-nav-link' : '' }}">
+            <i class="fas fa-tachometer-alt mr-3"></i>
+            Dashboard
+        </a>
 
             <div x-data="{ open: false }">
                 <a @click="open = !open" class="flex items-center  text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item pr-2">
-                <i class="fas fa-sticky-note mr-3"></i>
+                <i class="fas fa-sticky-note mr-3"></i>-
                 Data
                 <i class="fas fa-chevron-down ml-auto"></i>
                 </a>
@@ -30,6 +31,7 @@
             Matriks
         </a>
 
+    </div>
 
     </nav>
 </aside>
